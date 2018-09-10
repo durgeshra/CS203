3086583cdbf7794f3573cc358bd472478435e52f
Durgesh Agrawal <33284183+durgeshra@users.noreply.github.com>
2018-09-10 11:22:56 +0530

    Comment the code

--- a3.cpp
+++ a3.cpp
@@ -1,3 +1,7 @@
+//Report of Assignment 3 of CS203B (Fall, 2018)
+//Author: Durgesh Agrawal, 170262
+//Please visit https://moodle.cse.iitk.ac.in/pluginfile.php/13170/mod_resource/content/1/assignment-3-CS203B-2018.pdf for the questions
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -5,22 +9,22 @@ bool twop(int n)
 { 
 	if (n<=1) return true;
    return (ceil(log2(n)) == floor(log2(n))); 
-} 
+} //returns if a number is a power of 2 or not
 
 void q1a(){
 
 	int n=20;
 	long double arr[n];
-	for(int i=0;i<n;i++) arr[i]=0;
+	for(int i=0;i<n;i++) arr[i]=0;	//array that stores probabilities
 
 	long long int iter = 10000000;
-	srand(time(NULL));
-	clock_t tStart = clock();
+	srand(time(NULL));	//seed
+	clock_t tStart = clock();	//to keep a track of time
 
 	for(int j=0;j<iter;j++){
-		for(int i=1;i<n;i++){
+		for(int i=1;i<n;i++){	//iterate for each initial position
 			int pos = i;
-			while(pos!=0 && pos!=n){
+			while(pos!=0 && pos!=n){	//run until drunkard reaches either his home or the bar
 				int delta = (int)(rand())%2;
 				if (delta==0) pos--;
 				else pos++;
@@ -34,7 +38,7 @@ void q1a(){
 	}
 	printf("\n Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
 
-}
+}	//code for Q1 part (a)
 
 void q1b(){
 
@@ -42,78 +46,78 @@ void q1b(){
 	p_i[0]=0;
 	p_i[1]=0;
 
-
 	long double arr[25];
-	for(int l=0;l<25;l++) arr[l]=0;
+	for(int l=0;l<25;l++) arr[l]=0;	//ith entry stores probability corresponding to pow(2,i) steps
 
 	long long int iter = 10000;
-	long long int steps = 1000000;
+	long long int steps = 1000000;	//max limit on steps
 
-	srand(time(NULL));
-	clock_t tStart = clock();
+	srand(time(NULL));	//seed
+	clock_t tStart = clock();	//to keep a track of time
 
 	for(int j=0;j<iter;j++){
 		for(int i=0;i<2;i++){
-			long int pos = pow(10,i);
+			long int pos = pow(10,i);	//for i=1 and i=10
 			long int count = 0;
-			while(count<=steps){
+			while(count<=steps){	//do not exceed max limit on steps
 				count++;
 				int delta = (int)(rand())%2;
 				if (delta==0) pos--;
 				else pos++;
-				if(pos==0) break;
+				if(pos==0) break;	//drunkard reaches home
 			}
 			if (pos==0) {p_i[i]+=1; if(i==1 && count >0){ if (twop(count)==true) arr[(int)log2(count)]++; else arr[(int)log2(count)+1]++ ; }}
+			//update the array on the basis of counts required
 		}
 	}
 
 	cout << p_i[0]/iter << ' ' << p_i[1]/iter << ' ' << steps << '\n';
 	printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
-	
+
 	long long int temp = arr[0];
 
 	for(int l=1;l<25;l++){
 		temp+=arr[l];
 		arr[l]=temp;
-	}
+	}	//cumulatively add array elements since ith entry means that steps required are less than or equal to pow(2,i)
 	cout << '[';
 	for(int l=0;l<25;l++){
 		cout << arr[l]/iter << ',' ;
 	}
-	cout << ']';
+	cout << ']' << '\n';
 
 	cout << p_i[1];
 
-}
+}	//code for Q1 part (b)
 
 void q2(){
 
 	long double arr[50];
-	for(int l=0;l<50;l++) arr[l]=0;
+	for(int l=0;l<50;l++) arr[l]=0;	//ith entry stores probability corresponding to pow(2,i) steps
 
 	long double p_i=0;
 
 	long long int iter = 1000;
-	long long int steps = 1000000000;
+	long long int steps = 1000000000;	//max limit on steps
 
 	srand(time(NULL));
-	clock_t tStart = clock();
+	clock_t tStart = clock();	//to keep a track of time
 
 	for(int j=0;j<iter;j++){
 		long int posx = 1;
 		long int posy = 0;
 		long int count = 0;
-		while(count<=steps){
+		while(count<=steps){	//do not exceed max limit on steps
 			count++;
 			int delta = (int)(rand())%4;
 			if (delta==0) posx--;
 			else if (delta==1) posx++;
 			else if (delta==2) posy--;
 			else posy++;
-			if(posx==0 && posy==0) break;
+			if(posx==0 && posy==0) break;	//particle reaches origin
 		}
 			if (posx==0 && posy==0) {p_i+=1;  if (twop(count)==true) arr[(int)log2(count)]++; else arr[(int)log2(count)+1]++ ; }
-		
+			//update the array on the basis of counts required
 	}
 
 	long long int temp = arr[0];
@@ -121,32 +125,32 @@ void q2(){
 	for(int l=1;l<50;l++){
 		temp+=arr[l];
 		arr[l]=temp;
-	}
+	}	//cumulatively add array elements since ith entry means that steps required are less than or equal to pow(2,i)
 	cout << '[';
 	for(int l=0;l<50;l++){
 		cout << arr[l]/iter << ',' ;
 	}
-	cout << ']';
+	cout << ']' << '\n';
 
 	cout << p_i/iter << ' ' << steps << '\n';
 
 	printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
 
-}
+}	//code for Q2
 
 void q3(){
 
 
 	long double arr[30];
-	for(int l=0;l<30;l++) arr[l]=0;
+	for(int l=0;l<30;l++) arr[l]=0;	//ith entry stores probability corresponding to pow(2,i) steps
 
 	long double p_i=0;
 
 	long long int iter = 1000;
-	long long int steps = 10000000;
+	long long int steps = 10000000;	//max limit on steps
 
-	srand(time(NULL));
-	clock_t tStart = clock();
+	srand(time(NULL));	//seed
+	clock_t tStart = clock();	//to keep a track of time
 
 	for(int j=0;j<iter;j++){
 		long int posx = 1;
@@ -154,9 +158,8 @@ void q3(){
 				long int posz = 0;
 
 		long int count = 0;
-		while(count<=steps){
-						count++;
-
+		while(count<=steps){	//do not exceed max limit on steps
+			count++;
 			int delta = (int)(rand())%6;
 			if (delta==0) posx--;
 			else if (delta==1) posx++;
@@ -164,10 +167,10 @@ void q3(){
 			else if (delta==3) posy++;
 			else if (delta==4) posz--;
 			else posz++;
-			if(posx==0 && posy==0 & posz==0) break;
+			if(posx==0 && posy==0 & posz==0) break;	//particle reaches origin
 		}
 			if (posx==0 && posy==0 && posz==0) {p_i+=1;  if (twop(count)==true) arr[(int)log2(count)]++; else arr[(int)log2(count)+1]++ ; }
-		
+			//update the array on the basis of counts required
 	}
 
 	long long int temp = arr[0];
@@ -175,7 +178,8 @@ void q3(){
 	for(int l=1;l<30;l++){
 		temp+=arr[l];
 		arr[l]=temp;
-	}
+	}	//cumulatively add array elements since ith entry means that steps required are less than or equal to pow(2,i)
+
 	cout << '[';
 	for(int l=0;l<30;l++){
 		cout << arr[l]/iter << ',' ;
@@ -186,7 +190,7 @@ void q3(){
 
 	printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
 
-}
+}	//code for Q3
 
 int main(){
 
